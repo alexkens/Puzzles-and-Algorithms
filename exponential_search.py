ce5b117710ae4565b0e46dfa830bdd56a11ab8e0
andy_wendy <alexkens93@gmail.com>
2024-11-30 23:41:56 +0100

    Finish exponential search

--- exponential_search.py
+++ exponential_search.py
@@ -1,28 +1,44 @@
 
-def exponential_search(sorted_list, target_number):
+def exponential_search_iterative(sorted_list, target_number):
     
     basis = 0
     exponent = 2
     index = 0
     
     while index < len(sorted_list):
-        index = basis ** exponent
-
-        print(basis, exponent, index)
+        
+        print(f"Basis: {basis}, Index: {index}, current Number: {sorted_list[index]}")
 
         if sorted_list[index] == target_number:
             return 1
         elif sorted_list[index] < target_number:
             basis += 1
         else:
-            # linear search
-            for i in range(index, len(sorted_list)):
+            # linear search if sorted_list[index] > target_number
+            start = index - (basis**exponent - (basis-1)**exponent)
+            print(f"Commence linear search in the interval [{start} to {len(sorted_list)}]")
+            for i in range(start, len(sorted_list)):
                 if sorted_list[i] == target_number:
+                   print(f"{i - start} steps were taken")
                    return 1
+
+        # index operation
+        index = basis ** exponent - 1 if index > 0 else basis ** exponent
+        if index == 0: return -1
+    
+    start = index - (basis**exponent - (basis-1)**exponent)
+    print(f"Commence linear search in the interval [{start} to {len(sorted_list)}]")
+    for i in range(start, len(sorted_list)):
+        print(i)
+        if sorted_list[i] == target_number:
+            print(f"{i - start} steps were taken")
+            return 1
+                
     return -1
 
 
-sorted_list = [i+1 for i in range(11)]
-target_number = 4
-result = exponential_search(sorted_list, target_number)
+sorted_list = [i+1 for i in range(100)]
+target_number = 101
+print(sorted_list, " --> ", target_number)
+result = exponential_search_iterative(sorted_list, target_number)
 print(result)
