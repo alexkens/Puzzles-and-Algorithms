07faf8a78ed15bee1c5d3b7d7cf665ea22713c56
andy_wendy <alexkens93@gmail.com>
2024-12-20 15:04:45 +0100

    add two pointer technique

--- two_sum_problem.py
+++ two_sum_problem.py
@@ -4,7 +4,7 @@ import random
 import time
 
 
-def my_first_solution(array: list[int], target: int):
+def trivial_solution(array: list[int], target: int):
 
     length = len(array)
     for first in range(length-1):
@@ -12,6 +12,29 @@ def my_first_solution(array: list[int], target: int):
             if array[first] + array[second] == target:
                 return array[first], array[second]
     return -1, None
+
+
+def two_pointers_technique(array: list[int], target: int):
+
+    end = len(array) - 1
+    for i in range(len(array)):
+        if array[i] >= target:
+            end = i
+            break
+    
+    start = 0
+    while start < end:
+        current_sum = array[start] + array[end]
+        if current_sum == target:
+            return array[start], array[end]
+        if current_sum < target:
+            start += 1
+            continue
+        if current_sum > target:
+            end -= 1
+    
+    return -1, None
+
 
 def check(func):
     for i in range(10):
@@ -21,15 +44,18 @@ def check(func):
         target = random.randint(0, 100)
         for _ in range(10):
             array.append(random.randint(0, 100))
+        array = sorted(array)
         first, second = func(array, target)
         if second != None:
             print(f"{array} --> {target} --> {first}, {second}")
         else:
             print(f"{array} --> {target} --> No")
+    print()
 
 
-check(my_first_solution)
+check(trivial_solution)
+check(two_pointers_technique)
 
 """array = [1, 3, 5, 10, 20, 21]
-res = my_first_solution(array, 4)
+res = two_pointers_technique(array, 21)
 print(res)"""
