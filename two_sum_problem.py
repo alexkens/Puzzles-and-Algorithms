24e641ef5390e7372167e398f2299f0f4e6aa645
andy_wendy <alexkens93@gmail.com>
2024-12-20 13:58:23 +0100

    add check

--- two_sum_problem.py
+++ two_sum_problem.py
@@ -1,17 +1,35 @@
 # find pair sum in a sorted array
 
+import random
+import time
+
+
 def my_first_solution(array: list[int], target: int):
 
     length = len(array)
-
     for first in range(length-1):
         for second in range(first+1, length):
-            print(first, second)
             if array[first] + array[second] == target:
-                return 1 
-    return -1
+                return array[first], array[second]
+    return -1, None
+
+def check(func):
+    for i in range(10):
+        seed_time = time.time() * 5372654932**i
+        random.seed(seed_time)
+        array = []
+        target = random.randint(0, 100)
+        for _ in range(10):
+            array.append(random.randint(0, 100))
+        first, second = func(array, target)
+        if second != None:
+            print(f"{array} --> {target} --> {first}, {second}")
+        else:
+            print(f"{array} --> {target} --> No")
 
 
-array = [1, 3, 5, 10, 20, 21]
+check(my_first_solution)
+
+"""array = [1, 3, 5, 10, 20, 21]
 res = my_first_solution(array, 4)
-print(res)
+print(res)"""
